5ed327ce5075668f3d0a801040544cf3ade4d2f1
Jhanvi Vaishnav <jhanvi.vaishnav00@gmail.com>
2024-03-14 10:43:12 +0530

    :rocket: [add] assets

---
+++ lib/utils/urban_culture_images.dart
@@ -0,0 +1,5 @@
+class ImgConstatnts {
+  static String icCamera = "assets/icons/ic_camera.png";
+  static String icRoutine = "assets/icons/ic_routine.png";
+  static String icStreak = "assets/icons/ic_streak.png";
+}
